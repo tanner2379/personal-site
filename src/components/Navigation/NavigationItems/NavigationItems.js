4a0eae1787a1790d05ad0f004990d735b58f3f43
Tanner Sigel <tanner@barnpros.com>
2022-01-11 11:30:37 -0800

    added new logo and resume link

--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -1,21 +1,25 @@
-import React from 'react';
+import React from "react";
 
-import NavigationItem from './NavigationItem/NavigationItem';
+import NavigationItem from "./NavigationItem/NavigationItem";
 
-import classes from './NavigationItems.module.css';
+import classes from "./NavigationItems.module.css";
 
-const NavigationItems = props => {
+const NavigationItems = (props) => {
   return (
-    <ul className={classes.NavigationItems} >
-      <NavigationItem link={'/'} exact>
+    <ul className={classes.NavigationItems}>
+      <NavigationItem link={"/"} exact>
         About
       </NavigationItem>
-      <NavigationItem link={'/skills'} >
-        Skills
-      </NavigationItem>
-      <NavigationItem link={'/contact'} >
-        Contact
-      </NavigationItem>
+      <NavigationItem link={"/skills"}>Skills</NavigationItem>
+      <NavigationItem link={"/contact"}>Contact</NavigationItem>
+      <a
+        className={classes.navItemClone}
+        href={
+          "https://www.dropbox.com/s/cwerzwpytyxm9sw/Resume%20WebDev%202021.pdf?dl=0"
+        }
+      >
+        Resume
+      </a>
     </ul>
   );
 };
